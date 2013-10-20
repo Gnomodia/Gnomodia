6964b16dde27ef5c32fda3bb5b1d6b96eb570e1c
Alexander Krivács Schrøder <alexschrod@gmail.com>
2013-10-20 10:29:04 +0200

    * Explicitly license GnomodiaUI and GnomodiaUI.Controls as GPLv3. * Explicitly license Gnomodia as LGPLv3.

--- Gnomodia/Utility/FileExtensions.cs
+++ Gnomodia/Utility/FileExtensions.cs
@@ -1,3 +1,23 @@
+/*
+ *  Gnomodia
+ *
+ *  Copyright © 2013 Faark (http://faark.de/)
+ *  Copyright © 2013 Alexander Krivács Schrøder (https://alexanderschroeder.net/)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.IO;
 using System.Security.Cryptography;
